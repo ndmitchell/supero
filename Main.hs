cd630c532ee5cef8e3c54a3f6b6be08a85aabea5
Neil Mitchell <ndmitchell@gmail.com>
2006-12-07 19:54:08 +0000

    Add inlining to the result

--- Main.hs
+++ Main.hs
@@ -12,10 +12,14 @@ main = do
     let n = if null xs then 1 else read (head xs)
     pm <- loadCore "Primitive.ycr"
     cr <- loadCore x
-    let core = coreReachable ["main"] $ coreOverlay cr pm
+    let core = coreReach $ coreOverlay cr pm
         prog = optimise n $ convert core
-        core2 = coreReachable ["main"] $ revert core prog
+        core2 = coreReach $ coreInlin $ coreReach $ revert core prog
     print core2
+
+
+coreReach = coreReachable ["main"]
+coreInlin = coreInline Basic
 
 
 optimise :: Int -> Prog -> Prog

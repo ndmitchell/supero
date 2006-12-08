531077525cbe122a5ae0639eeee892933be0c600
Neil Mitchell <ndmitchell@gmail.com>
2006-12-08 21:21:11 +0000

    Add some extra primitives

--- example/Primitive.hs
+++ example/Primitive.hs
@@ -3,8 +3,4 @@
 
 module Primitive where
 
-foreign import primitive global_Prelude'_error :: a -> b
-foreign import primitive global_System'_IO'_stdout :: a
-foreign import primitive global_System'_IO'_stdin :: a
-foreign import primitive global_System'_IO'_stderr :: a
-foreign import primitive global_System'_IO'_hPutChar :: a -> b -> c
+foreign import primitive global_Prelude'_error :: String -> a

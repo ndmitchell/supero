a8fa39a42ef2eace02a8690a8dda8be45cacef08
Neil Mitchell <ndmitchell@gmail.com>
2006-12-18 13:16:58 +0000

    Fix the primitives, was in conflict before

--- example/Primitive.hs
+++ example/Primitive.hs
@@ -3,4 +3,8 @@
 
 module Primitive where
 
-foreign import primitive global_Prelude'_error :: String -> a
+foreign import primitive global_Prelude'_error :: a -> b
+foreign import primitive global_System'_IO'_stdout :: a
+foreign import primitive global_System'_IO'_stdin :: a
+foreign import primitive global_System'_IO'_stderr :: a
+foreign import primitive global_System'_IO'_hPutChar :: a -> b -> c

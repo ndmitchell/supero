0fe06cf88903fe5e33195a3f5a4ec7ef5961617e
Neil Mitchell <ndmitchell@gmail.com>
2007-05-01 19:26:44 +0000

    Remove hacky short-cut, not needed now with much faster binary library

--- Main.hs
+++ Main.hs
@@ -9,14 +9,5 @@ main = do
     core <- loadCore "Example.yca"
     over <- loadCore "library/Overlay.ycr"
     core <- return $ coreReachable ["main"] $ coreOverlay core over
-    saveCore "_generated.yca" core
-    results core
-
-
-results core = do
     writeFile "_generated.txt" $ show core
     generate "_generated.hs" core
-
-quickmain = do
-    core <- loadCore "_generated.yca"
-    results core

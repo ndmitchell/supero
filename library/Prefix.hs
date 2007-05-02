0e565ca2d3d284164888dd9b8cbf10f54cdb3e84
Neil Mitchell <ndmitchell@gmail.com>
2007-05-02 16:01:13 +0000

    Add various tweaks to the prefix, so it should actually not CAF with -O2

--- library/Prefix.hs
+++ library/Prefix.hs
@@ -1,15 +1,23 @@
+{-# OPTIONS_GHC -fffi #-}
 
 import System.IO.Unsafe
 import System.IO
+import Data.Word
 
 main = main_main `seq` (return () :: IO ())
 
+{-# NOINLINE wrapIO #-}
 wrapIO x = unsafePerformIO (x >>= return . Overlay_IO)
 
 p_System_IO_hPutChar h c = wrapIO (hPutChar h (toEnum c))
-p_System_IO_hGetChar h   = wrapIO $ do
-    b <- hIsEOF h
-    if b then return 0 else hGetChar h >>= return . chr_
+p_System_IO_hGetChar h   = wrapIO (getCharIO h)
+
+foreign import ccall "stdio.h getchar" getchar :: IO Word8
+
+{-# NOINLINE getCharIO #-}
+getCharIO h = do
+    c <- getchar
+    return $ if c == (-1) then 0 else chr_ c
 
 p_Prelude_seq = seq
 
@@ -32,4 +40,3 @@ str_ x = map chr_ x
 
 p_System_IO_stdin = stdin
 p_System_IO_stdout = stdout
-

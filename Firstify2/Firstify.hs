44957842429b6738ec3fa52a53c08a865fdcb6be
Neil Mitchell <ndmitchell@gmail.com>
2007-05-14 16:13:37 +0000

    Make the firstify thing do a case transformation

--- Firstify2/Firstify.hs
+++ Firstify2/Firstify.hs
@@ -2,9 +2,12 @@
 module Firstify2.Firstify where
 
 import Yhc.Core
+import Yhc.Core.Play2
 import Unique
 import Firstify2.SpecExpr
 import Firstify2.SpecState
+import Control.Monad.State
+import Data.List
 
 
 firstify :: Core -> Core
@@ -17,4 +20,24 @@ firstifyData = coreReachable ["main"] . specMain True specExpr
 
 
 firstifyDataPrepare :: Core -> Core
-firstifyDataPrepare = id
+firstifyDataPrepare core = core{coreFuncs = newfuncs ++ oldfuncs}
+    where
+        (oldfuncs,(uid,newfuncs)) = runState (mapM f $ coreFuncs core) (uniqueFuncsMin core,[])
+
+        f (CoreFunc name args body) = liftM (CoreFunc name args) $ traverseCoreM (g name) body
+        f x = return x
+        
+        g name (CoreCase on alts) = liftM (CoreCase on) $ mapM (h name) alts
+        g name x = return x
+        
+        h name (CoreApp (CoreCon c) args, rhs) | not $ null args = do
+            (uid,fs) <- get
+            let newname = uniqueName name uid
+                vars = map fromCoreVar args
+                free = collectFreeVars rhs \\ vars
+                newargs = free ++ vars
+                expr = (CoreApp (CoreCon c) args, CoreApp (CoreFun newname) (map CoreVar newargs))
+                func = CoreFunc newname newargs rhs
+            put (uid+1, func:fs)
+            return expr
+        h name x = return x

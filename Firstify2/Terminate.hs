95b2dfbec562005ecbfa38e518ad54ec7a7b0737
Neil Mitchell <ndmitchell@gmail.com>
2007-05-14 09:48:15 +0000

    Weaken the constraint to anything which is a prefix of the bad function

--- Firstify2/Terminate.hs
+++ Firstify2/Terminate.hs
@@ -2,11 +2,12 @@
 module Firstify2.Terminate(weakenTemplate) where
 
 import Firstify2.SpecState
+import Data.List
 import Debug.Trace
 
 
 weakenTemplate :: Template -> Spec (Maybe Template)
-weakenTemplate (Template n [x,y]) | n == name = return $ Just $ Template name [x,TempNone]
+weakenTemplate (Template n (x1:y:xs)) | name `isPrefixOf` n = return $ Just $ Template n (x1:TempNone:xs)
     where name = "Prelude.Prelude.Prelude.1107.showPosInt"
 
 weakenTemplate x = return $ Just x

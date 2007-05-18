51f56197dafce04ca6f95b41dc6a7bd0cd0b8653
Neil Mitchell <ndmitchell@gmail.com>
2007-05-18 14:17:22 +0000

    Use getArgs to indicate which program we are working on

--- Main.hs
+++ Main.hs
@@ -11,34 +11,35 @@ import LambdaLift
 import Report
 import Unique
 import System.Directory
+import System.Environment
 
 
 main = do
-    createDirectoryIfMissing True "generated"
-    core <- loadCore "Example.yca"
+    [file] <- getArgs
+    createDirectoryIfMissing True file
+    core <- loadCore (file ++ ".yca")
     over <- loadCore "library/Overlay.ycr"
     core <- return $ uniqueFuncs $ removeSeq $ traverseCore remCorePos $ coreReachable ["main"] $ coreOverlay core over
-    output 1 core
+    output file 1 core
 
     putStrLn "Firstifying basic"
     core <- return $ F2.firstify core
-    output 2 core
+    output file 2 core
     putStrLn $ unlines $ report core
 
-
     core <- return $ F2.firstifyDataPrepare core
-    output 3 core
+    output file 3 core
 
     putStrLn "Firstifying scary"
     core <- return $ F2.firstifyData core
-    output 4 core
+    output file 4 core
     putStrLn $ unlines $ report core
 
 
-output n core = do
+output file n core = do
     let sn = show n
-    writeFile ("generated/" ++ sn ++ "__.hs") $ show core
-    generate  ("generated/" ++ sn ++ ".hs"  ) core
+    writeFile (file ++ "/" ++ sn ++ "__.hs") $ show core
+    generate  (file ++ "/" ++ sn ++ ".hs"  ) core
 
 
 removeSeq x = traverseCore f x

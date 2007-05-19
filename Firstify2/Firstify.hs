09914827ee47b4b113b118b6c9c14db3c07e09a0
Neil Mitchell <ndmitchell@gmail.com>
2007-05-19 23:05:28 +0000

    Add a preprocess stage to move let's and cases around

--- Firstify2/Firstify.hs
+++ Firstify2/Firstify.hs
@@ -24,8 +24,9 @@ coreFix = coreReachable ["main"] . coreInline InlineForward
 
 
 firstifyDataPrepare :: Core -> Core
-firstifyDataPrepare core = core{coreFuncs = newfuncs ++ oldfuncs}
+firstifyDataPrepare core2 = core{coreFuncs = newfuncs ++ oldfuncs}
     where
+        core = traverseCore simp $ coreSimplify core2
         (oldfuncs,(uid,newfuncs)) = runState (mapM f $ coreFuncs core) (uniqueFuncsMin core,[])
 
         f (CoreFunc name args body) = liftM (CoreFunc name args) $ traverseCoreM (g name) body
@@ -45,3 +46,32 @@ firstifyDataPrepare core = core{coreFuncs = newfuncs ++ oldfuncs}
             put (uid+1, func:fs)
             return expr
         h name x = return x
+
+
+simp x@(CoreCase (CoreVar on) alts) | on `elem` collectFreeVars (CoreCase (CoreInt 0) alts) =
+        CoreCase (CoreVar on) (map f alts)
+    where
+        f (lhs,rhs) = (lhs, replaceFreeVars [(on,lhs)] rhs)
+
+simp (CoreLet [] x) = x
+simp (CoreLet ((a,b):bs) x) = reduceBind a b $ simp $ CoreLet bs x
+
+simp x = x
+
+
+
+reduceBind lhs rhs (CoreCase on alts) | lhs `notElem` collectFreeVars on = simp $ CoreCase on $ map f alts
+    where
+        f (alhs,arhs) | lhs `notElem` collectFreeVars alhs = (alhs, reduceBind lhs rhs arhs)
+        f x = x
+
+reduceBind lhs rhs x = CoreLet [(lhs,rhs)] x
+
+
+{-
+simp (CoreLet ((a,b):bs) x) = reduceLet a b $ simp $ CoreLet bs x
+
+| not $ null yes = coreLet no $ traverseCore simp $ replaceFreeVars yes x
+    where
+        (yes,no) = partition ((<= 1) . flip countFreeVar x . fst) binds
+-}

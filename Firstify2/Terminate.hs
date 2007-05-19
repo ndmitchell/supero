dba6728cd36f5e17b8fe69212251474bf87c97c0
Neil Mitchell <ndmitchell@gmail.com>
2007-05-19 00:31:58 +0000

    Make the terminate thing dump out better error messages

--- Firstify2/Terminate.hs
+++ Firstify2/Terminate.hs
@@ -4,6 +4,8 @@ module Firstify2.Terminate(weakenTemplate) where
 import Firstify2.SpecState
 import Data.List
 import Debug.Trace
+import Control.Monad.State
+import qualified Data.Map as Map
 
 
 weakenTemplate :: Template -> Spec (Maybe Template)
@@ -12,4 +14,22 @@ weakenTemplate (TemplateApp n xs@(_:_)) | name `isPrefixOf` n = return $ Just $ 
         name = "Prelude.Prelude.Prelude.1107.showPosInt"
         xs2 = init xs ++ [TempNone]
 
-weakenTemplate x = return $ Just x
+weakenTemplate x = do
+    s <- get
+    if uid s > 1000 then
+        error $ unlines $ map showTemplate $ Map.toList $ template s
+     else
+        return $ Just x
+
+
+showTemplate (t, y) = y ++ " = " ++ f1 t
+    where
+        f1 (TemplateApp name args) = unwords $ name : map f2 args
+        f1 (TemplateCase name extra alts) = "case " ++ f2 (TempApp name extra) ++ " of " ++ concatMap f3 alts
+        
+        f2 (TempApp name extra) = name ++ " #" ++ show extra
+        f2 TempNone = "_"
+        f2 (TempCon c args) = "(" ++ unwords (c : map f2 args) ++ ")"
+
+        f3 ("",x) = "_ -> " ++ f2 x ++ "; "
+        f3 (c ,x) = c ++ " -> " ++ f2 x ++ "; "

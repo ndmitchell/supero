190b60a5b15ba8c0be55ea7b5352297a91585c76
Neil Mitchell <ndmitchell@gmail.com>
2007-07-09 22:26:10 +0000

    Run the C and Supero tests last, and twice each

--- bench.bat
+++ bench.bat
@@ -11,12 +11,20 @@ ghc -optc-O3 Example.c -odir obj\c -o c.exe
 
 ghc --make -O2 ExampleSingle.hs -o haskell.exe -hidir obj\haskell -odir obj\haskell
 
+echo Benchmarking Haskell
+type %data% | timer haskell
+echo.
+
 echo Benchmarking C
 type %data% | timer c
 echo.
 
-echo Benchmarking Haskell
-type %data% | timer haskell
+echo Benchmarking Supero
+type %data% | timer supero
+echo.
+
+echo Benchmarking C
+type %data% | timer c
 echo.
 
 echo Benchmarking Supero

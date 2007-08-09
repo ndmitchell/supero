9a9b267f679a37b0bc8252ddefcc471999a022d6
Neil Mitchell <ndmitchell@gmail.com>
2007-08-09 02:24:32 +0000

    Change to Evaluator2, which has different termination behaviour, still not right

--- Main.hs
+++ Main.hs
@@ -11,14 +11,14 @@ import System.Directory
 import System.Environment
 import System.Cmd
 
-import Evaluate
+import Evaluate2
 
 main = do
     [file] <- getArgs
     system $ "yhc test/" ++ file ++ "/" ++ file ++ ".hs --linkcore --hide"
     core <- loadCore ("test/" ++ file ++ "/ycr/" ++ file ++ ".yca")
     over <- loadCore "library/Overlay.ycr"
-    core <- return $ transs $ coreReachable ["main"] $ liftMain $ coreOverlay core over
+    core <- return $ coreReachable ["main"] $ transs $ coreReachable ["main"] $ liftMain $ coreOverlay core over
     output file 1 core
     
     output file 4 (evaluate core)
@@ -42,9 +42,11 @@ main = do
 
 output file n core = do
     let sn = show n
+    saveCore  ("test/" ++ file ++ "/" ++ sn ++ ".ycr") core
     writeFile ("test/" ++ file ++ "/" ++ sn ++ "__.hs") $ show core
     generate  ("test/" ++ file ++ "/" ++ sn ++ ".hs"  ) core
     writeFile ("test/" ++ file ++ "/" ++ sn ++ ".html") $ coreHtml core
+    putStrLn $ "Written file " ++ show n
 
 
 transs = ensureInvariants [ConsecutiveFuncs, NoCorePos, NoRecursiveLet, NoCaseDefaultOne]

6ae0181542a07152bc051899ea0502c06ed3bb1f
Neil Mitchell <ndmitchell@gmail.com>
2007-08-14 17:57:21 +0000

    Add a simple nqueens thing, with a slightly reduced computation

---
+++ test/test/Test.hs
@@ -0,0 +1,24 @@
+
+module Test where
+
+-- !!! count the number of solutions to the "n queens" problem.
+-- (grabbed from LML dist)
+
+main nq = nsoln nq
+
+
+{-
+main = do
+    [arg] <- getArgs
+    print $ nsoln $ read arg
+-}
+
+nsoln nq = length (gen nq)
+    where
+        safe :: Int -> Int -> [Int] -> Bool
+        safe x d []    = True
+        safe x d (q:l) = x /= q && safe x (d+1) l
+
+        gen :: Int -> [[Int]]
+        gen 0 = [[]]
+        gen n = [ (q:b) | b <- gen (n-1), q <- [1..nq], safe q 1 b]

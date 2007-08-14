8560ddc0a081e8ef1917ecdfdbeda8328fb98623
Neil Mitchell <ndmitchell@gmail.com>
2007-08-14 17:59:19 +0000

    Upgrade the state monad to include IO in the list

--- StateFail.hs
+++ StateFail.hs
@@ -6,14 +6,16 @@ import Control.Monad.Error
 import Yhc.Core.UniqueId
 
 
-type StateFail state failure result = StateT state (Either String) result
+type StateFail state failure result = StateT state (ErrorT String IO) result
 
 
-sfRun :: (Show failure, Read failure) => StateFail state failure result -> state -> Either failure (result,state)
-sfRun x state = case runStateT x state of
-                    Left failure -> Left (read failure)
-                    Right (result,state) -> Right (result,state)
 
+sfRun :: (Show failure, Read failure) => StateFail state failure result -> state -> IO (Either failure (result,state))
+sfRun x state = do
+    res <- runErrorT (runStateT x state)
+    return $ case res of
+         Left failure -> Left (read failure)
+         Right (result,state) -> Right (result,state)
 
 sfFail :: Show failure => failure -> StateFail state failure result
 sfFail failure = lift $ fail (show failure)
@@ -22,4 +24,7 @@ sfFail failure = lift $ fail (show failure)
 instance (Monad m, UniqueId i) => UniqueIdM (StateT i m) where
     getIdM = liftM getId get
     putIdM n = modify (putId n)
+
+sfPrint :: String -> StateFail state failure ()
+sfPrint s = liftIO $ putStrLn s
 

753611c3ac02236d49b4ac3786bd4b013cd786a7
Neil Mitchell <ndmitchell@gmail.com>
2007-09-06 23:32:45 +0000

    Add lca to the Termination module

--- Termination.hs
+++ Termination.hs
@@ -2,6 +2,9 @@
 module Termination where
 
 import Yhc.Core
+import Yhc.Core.UniqueId
+import Yhc.Core.FreeVar3
+import Control.Monad
 import CoreUtil
 
 
@@ -12,10 +15,22 @@ msg = undefined
 
 
 
+-- | Homeomorphic embedding
 (<<|) :: CoreExpr -> CoreExpr -> Bool
--- rule ommitted as I don't do Math's operations
--- CoreLit x <<| CoreLit y = True
+CoreLit _ <<| CoreLit _ = True -- can omit as I don't do maths?
 CoreVar _ <<| CoreVar _ = True
 x <<| y =
     any (x <<|) (children y) ||
     (x `eq1` y && and (zipWith (<<|) (children x) (children y)))
+
+
+
+-- | Least common anti-instance
+lca :: UniqueIdM m => CoreExpr -> CoreExpr -> m (CoreExpr, Subst, Subst)
+lca x y | x `eq1` y = do
+        let rep = snd $ uniplate x
+        (cs,sx,sy) <- liftM unzip3 $ zipWithM lca (children x) (children y)
+        return (rep cs, concat sx, concat sy)
+lca x y = do
+    v <- getVar
+    return (CoreVar v, [(v,x)], [(v,y)])

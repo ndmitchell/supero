bebda96ac674f03e147fdc62fb811cc315838dcc
Neil Mitchell <ndmitchell@gmail.com>
2007-11-07 16:47:17 +0000

    Add initial code to go through the benchmarks, not yet actually with the benchmark specific logic yet

--- Nofib.hs
+++ Nofib.hs
@@ -1,13 +1,48 @@
 
 module Nofib(nofib) where
 
+import Control.Monad
+import Data.Maybe
+import System.Cmd
+import System.Directory
+import System.FilePath
+
+import Options
+
+
+folders = ["imaginary","spectral","real"]
 
 type Benchmark = String
 
 --                                      errmsg executable
-type Compiler = Benchmark -> IO (Either String String)
+type Compiler = Options -> Benchmark -> IO (Either String String)
 
 
-nofib :: String -> [(String,Compiler)] -> [Benchmark] -> IO ()
-nofib root comps benchs = return ()
+nofib :: Options -> [(String,Compiler)] -> [Benchmark] -> IO ()
+nofib options comps benchs = do
+    benchs <- resolveBenchmarks options benchs
+    sequence_ [do
+        putStrLn $ "Running " ++ takeBaseName b ++ " with " ++ name
+        res <- c options b
+        case res of
+            Left err -> putStrLn $ "Doh: " ++ err
+            Right exec -> runBenchmark (takeBaseName b) exec
+        | b <- benchs, (name,c) <- comps]
 
+
+resolveBenchmarks :: Options -> [Benchmark] -> IO [Benchmark]
+resolveBenchmarks (Options {optNofibLocation=root}) bs = do
+    let f x = liftM (filter ('.' `notElem`)) $ getDirectoryContents (root </> x)
+    tests <- liftM (zip folders) $ mapM f folders
+
+    let f "." = concatMap snd tests
+        f x | x `elem` folders = fromJust $ lookup x tests
+        f x = let res = [x | a <- concatMap snd tests, takeBaseName a == x]
+              in if null res then error $ "Nofib test not found, " ++ x else res
+    return $ concatMap f bs
+
+
+runBenchmark :: Benchmark -> FilePath -> IO ()
+runBenchmark bench exe = do
+    system exe
+    putStrLn "Done"

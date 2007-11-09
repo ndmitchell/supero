be5c042a1eac698306c980b686aed3f833e474a9
Neil Mitchell <ndmitchell@gmail.com>
2007-11-09 21:03:09 +0000

    Move the CAF detection stuff into a separate module

--- Optimise/Evaluate.hs
+++ Optimise/Evaluate.hs
@@ -4,6 +4,7 @@ module Optimise.Evaluate(evaluate) where
 import Optimise.State
 import Optimise.Termination
 import Optimise.Util
+import Optimise.CAF
 
 import Yhc.Core hiding (uniqueBoundVarsFunc)
 import Yhc.Core.FreeVar3
@@ -52,7 +53,6 @@ coreFix :: Core -> Core
 coreFix = coreReachable ["main"] . coreInline InlineCallOnce
 
 
-
 ---------------------------------------------------------------------
 -- EVAL DRIVER
 
@@ -67,44 +67,7 @@ eval cafs core = do
         primsSet = Set.fromList $ map coreFuncName prims
 
 
----------------------------------------------------------------------
--- CAF DETECTION
-
-detectCafs :: Core -> Set.Set CoreFuncName
-detectCafs core = Set.fromList [coreFuncName x | x <- coreFuncs core, isCaf (coreFuncMap fm) x]
-    where fm = toCoreFuncMap core
-
-
-isCaf func (CoreFunc name [] body) = expensive $ coreSimplify body
-    where
-        expensive (CoreCon x) = False
-        expensive (CoreFun x) = False
-        expensive (CoreLit x) = False
-        expensive (CoreApp (CoreCon x) xs) = any expensive xs
-        expensive (CoreApp (CoreFun x) xs) = not $ unsaturated func x xs
-        expensive x = error $ show ("missed",x)
-
-isCaf _ _ = False
-
-
-unsaturated :: (CoreFuncName -> CoreFunc) -> CoreFuncName -> [CoreExpr] -> Bool
-unsaturated func name args = f [] name (length args)
-    where
-        f seen name args | name `elem` seen = False
-                         | args == 0 || arity > args = True
-                         | isCoreFunc x = g (name:seen) (coreFuncBody x) (args - arity)
-                         | otherwise = False
-            where
-                x = func name
-                arity = coreFuncArity x
-
-        g seen (CoreApp (CoreFun name) args) extra = f seen name (length args + extra)
-        g seen (CoreFun name) extra = f seen name extra
-        g seen (CorePos _ x) extra = g seen x extra
-        g _ _ _ = False
-
-
----------------------------------------------------------------------
+--------------------------------------------------------------------
 -- RECURSIVE TIE
 
 

051c7f8d5179dcf2904f92fb32f6b4aec69b94c2
Neil Mitchell <ndmitchell@gmail.com>
2007-11-09 15:26:56 +0000

    Add code to generate a report

--- Report.hs
+++ Report.hs
@@ -1,26 +1,60 @@
 
 module Report(report) where
 
-import Yhc.Core
-import qualified Data.Map as Map
+import Safe
+import Data.List
 import Data.Maybe
 
 
--- report every occurence of a non-saturated application
-report :: Core -> [String]
-report core = ["In " ++ name ++ ": " ++ x ++ " want:" ++ show arity ++ ", got:" ++ show app
-        | CoreFunc name _ bod <- coreFuncs $ alwaysAppFun core
-        , CoreApp (CoreFun x) xs <- universe bod
-        , let arity = (fromJust $ Map.lookup x table) :: Int
-        , let app = length xs
-        , arity /= app
-        ]
-    where
-        table = Map.fromList [(coreFuncName x, coreFuncArity x) | x <- coreFuncs core]
+-- The MR sucks!
+snub x = sort $ nub x
 
-alwaysAppFun :: Core -> Core
-alwaysAppFun = transformExpr f
+report :: IO ()
+report = do
+    src <- readFile "results.txt"
+    let res = map (\[a,b,c] -> (a,b,c)) $ map words $ lines src
+        (comps,tests,_) = unzip3 res
+    comps <- return $ snub comps
+    tests <- return $ snub tests
+
+    let f test = "<tr><td>" ++ test ++ "</td>" ++
+                 reportTest comps [(a,read c) | (a,b,c) <- res, b == test] ++
+                 "</tr>"
+
+    let ans = "<html><head><style type='text/css'>" ++ css ++ "</style></head><body><table>" ++
+              "<tr><td></td>" ++ concatMap (tag "td") comps ++ "</tr>" ++
+              concatMap f tests ++
+              "</table></body></html>"
+
+    writeFile "report.htm" ans
+
+
+reportTest :: [String] -> [(String,Integer)] -> String
+reportTest comps res = concatMap g vals
     where
-        f (CoreFun x) = CoreApp (CoreFun x) []
-        f (CoreApp (CoreApp x xs) ys) = CoreApp x (xs++ys)
-        f x = x
+        (low,high) = (minimum real, maximum real)
+        real = catMaybes vals
+        vals = map f comps
+        
+        f c = minimumMay [b | (a,b) <- res, a == c]
+
+        g Nothing = "<td></td>"
+        g (Just x) = "<td style='background-color:rgb(255," ++ red ++ "," ++ red ++ ")'>" ++ dp2 val ++ "</td>"
+            where
+                red = if val < 105 then "255" else
+                      show $ round $ (255*) $ (1-) $ fromInteger (x - low) / fromInteger (high - low) 
+                val = ((x * 100) `div` low)
+
+
+dp2 x = reverse whole ++ "." ++ reverse frac
+    where (frac,whole) = splitAt 2 $ reverse $ show x
+
+
+
+tag x y = "<" ++ x ++ ">" ++ y ++ "</" ++ x ++ ">"
+
+
+css = unlines
+    ["table {border: 1px solid black;}"
+    ]
+    

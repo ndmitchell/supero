a70d8165d3e317016d7c2a0adbb83f608a65c866
Neil Mitchell <ndmitchell@gmail.com>
2007-11-09 20:56:46 +0000

    Add system_ and Options into General

--- General.hs
+++ General.hs
@@ -1,9 +1,37 @@
 
-module General where
+module General(
+    Options(..), readOptions,
+    system_
+    ) where
 
-import Data.List
+import System.Directory
+import Control.Monad
+import Safe
+import System.Cmd
+import System.Exit
 
 
-unique :: Eq a => [a] -> Bool
-unique x = length x == length (nub x)
+data Options = Options {
+                   optNofibLocation :: FilePath,
+                   optObjLocation :: FilePath
+               }
+               deriving Show
+
+
+readOptions :: IO Options
+readOptions = do
+    nofib <- locate ["D:\\sources\\contrib\\nofib"]
+    obj <- locate ["D:\\Temp\\supero"]
+    return $ Options nofib obj
+
+
+locate opts = do
+    liftM
+        (headNote $ "Can't find a directory, wanted one of: " ++ show opts)
+        (filterM doesDirectoryExist opts)
+
+
+system_ cmd = do
+    res <- system cmd
+    when (res /= ExitSuccess) $ error $ "ERROR: System call failed\n" ++ cmd
 

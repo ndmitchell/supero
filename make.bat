0a61ba032f94853e52ded20120ae47f7cb87cef9
Neil Mitchell <ndmitchell@gmail.com>
2007-11-13 12:29:24 +0000

    Change the make.bat file, don't require -fglasgow-exts and create obj if it is missing

--- make.bat
+++ make.bat
@@ -1,6 +1,9 @@
+@echo off
+if not exist obj mkdir obj
+
 set libs=d:\sources\yhc\current\src\libraries\core
 if not exist %libs% set libs=C:\Documents\Uni\yhc\current\src\libraries\core
 
-if "%1"=="" ghc --make Main.hs -i%libs% -fglasgow-exts -hidir obj -odir obj -o supero.exe
-if "%1"=="i" ghci Main.hs -i%libs% -fglasgow-exts -hidir obj -odir obj
+if "%1"=="" ghc --make Main.hs -i%libs% -hidir obj -odir obj -o supero.exe
+if "%1"=="i" ghci Main.hs -i%libs% -hidir obj -odir obj
 

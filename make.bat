3a69d3e1b573b68d9d0b5f709929ec08a573de7c
Neil Mitchell <ndmitchell@gmail.com>
2007-11-20 19:22:40 +0000

    Add profiling to the make file

--- make.bat
+++ make.bat
@@ -1,5 +1,6 @@
 @echo off
 if not exist obj mkdir obj
+if not exist obj\prof mkdir obj\prof
 
 set libs=d:\sources\yhc\current\src\libraries\core
 if not exist %libs% set libs=C:\Documents\Uni\yhc\current\src\libraries\core
@@ -7,4 +8,4 @@ if not exist %libs% set libs=C:\Neil\yhc\src\libraries\core
 
 if "%1"=="" ghc --make Main.hs -i%libs% -hidir obj -odir obj -o supero.exe
 if "%1"=="i" ghci Main.hs -i%libs% -hidir obj -odir obj
-
+if "%1"=="p" ghc -prof -auto-all --make Main.hs -i%libs% -hidir obj\prof -odir obj\prof -o superop.exe

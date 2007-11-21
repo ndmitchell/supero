4664eff22305b6a63821841d50c5468f3815d165
Neil Mitchell <ndmitchell@gmail.com>
2007-11-21 02:22:42 +0000

    Add a make script for venice

---
+++ make.sh
@@ -0,0 +1,2 @@
+ghc -O2 --make Main.hs -i/grp/haskell/yhc/src/libraries/core/ -i/grp/haskell/cabal-packages/safe/ -i/grp/haskell/cabal-packages/uniplate/ -hidir /tmp/ndm/supero/comp/ -odir /tmp/ndm/supero/comp/ -fasm -o supero
+

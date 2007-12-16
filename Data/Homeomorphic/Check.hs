fe92c114a0f9e31bdcd56f7394129c535a6b09da
Neil Mitchell <ndmitchell@gmail.com>
2007-12-16 16:45:31 +0000

    Add Data.Homeomorphic.Check, which does both Simple and Neil and check's the results match

---
+++ Data/Homeomorphic/Check.hs
@@ -0,0 +1,25 @@
+
+module Data.Homeomorphic.Check where
+
+import qualified Data.Homeomorphic.Simple as H1
+import qualified Data.Homeomorphic.Neil   as H2
+import Data.Homeomorphic.Internal
+
+data Homeomorphic k v = Homeomorphic (H1.Homeomorphic k v) (H2.Homeomorphic k v)
+
+empty :: Homeomorphic k v
+empty = Homeomorphic H1.empty H2.empty
+
+
+insert :: Ord k => Shell k -> v -> Homeomorphic k v -> Homeomorphic k v
+insert k v (Homeomorphic h1 h2) = Homeomorphic (H1.insert k v h1) (H2.insert k v h2)
+
+
+-- additional Eq v constraint required to check the answers
+find :: (Eq v, Ord k) => Shell k -> Homeomorphic k v -> [v]
+find k (Homeomorphic h1 h2) =
+        if r1 == r2 then r1 else
+           error "Data.Homeomorhpic.Check.find: mismatch"
+    where
+        r1 = H1.find k h1
+        r2 = H2.find k h2

5cf2efbefcd3b742be3f72a1a46fe7ad378ac22b
Neil Mitchell <ndmitchell@gmail.com>
2007-12-16 23:37:57 +0000

    Make Data.Homeomorphic export findOne

--- Data/Homeomorphic.hs
+++ Data/Homeomorphic.hs
@@ -2,7 +2,7 @@
 module Data.Homeomorphic(
     Shell, shell,
     (<<|), couple, dive,
-    Homeomorphic, empty, insert, find
+    Homeomorphic, empty, insert, find, findOne
     ) where
 
 import Data.Homeomorphic.Simple

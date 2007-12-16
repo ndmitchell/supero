bb3e1982c35251473933be2f2bc339dd635fa2da
Neil Mitchell <ndmitchell@gmail.com>
2007-12-16 23:38:18 +0000

    Add Data.Homeomorphic.Hash, which keeps track of a hash

---
+++ Data/Homeomorphic/Hash.hs
@@ -0,0 +1,36 @@
+
+module Data.Homeomorphic.Hash where
+
+import qualified Data.Set as Set
+import Data.Homeomorphic.Internal
+import Data.Maybe
+
+data Homeomorphic k v = Homeomorphic [(Shell k, Hash k, v)]
+
+
+empty :: Homeomorphic k v
+empty = Homeomorphic []
+
+insert :: Ord k => Shell k -> v -> Homeomorphic k v -> Homeomorphic k v
+insert k v (Homeomorphic xs) = Homeomorphic ((k,calcHash k,v):xs)
+
+find :: Ord k => Shell k -> Homeomorphic k v -> [v]
+find k (Homeomorphic xs) = [c | (a,b,c) <- xs, check b, a <<| k]
+    where check = checkHash k
+
+findOne :: Ord k => Shell k -> Homeomorphic k v -> Maybe v
+findOne k = listToMaybe . find k
+
+
+
+---------------------------------------------------------------------
+
+type Hash k = Set.Set (Int,k)
+
+calcHash :: Ord k => Shell k -> Hash k
+calcHash = Set.fromList . f
+    where f (Shell a b c) = (b,a) : concatMap f c
+
+-- important: calculate the hash of y only once per invokation
+checkHash :: Ord k => Shell k -> Hash k -> Bool
+checkHash y = (`Set.isSubsetOf` calcHash y)

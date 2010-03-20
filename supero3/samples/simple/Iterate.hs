e96b014a6e5fca285f73c62d907bf439b2185be4
Neil Mitchell <ndmitchell@gmail.com>
2010-03-20 17:47:11 +0000

    Lots of changes

--- supero3/samples/simple/Iterate.hs
+++ supero3/samples/simple/Iterate.hs
@@ -1,37 +1,53 @@
 
 module Main where
 
+#if MAIN
 
-main n = {- map square -} (repeat 0) !! n
+main = print (root (400000 :: Int) :: Int)
 
-{-
-square x = x * x
+#endif
+
+
+root n = map square (iterate inc 1) !! n
 inc x = x + 1
-eq x = x == 0
--}
-dec x = x - 1
-eq x = prim (Equal x 0)
+square x = x * x
 
-repeat x = x : repeat x
+
+
+#if MAIN_SUPERO
+
+addInt'2 = (+)
+mulInt'2 = (*)
+eqInt'2 = (==)
+neqInt'2 = (/=) :: Int -> Int -> Bool
+modInt'2 = mod
+subInt'2 = (-)
+error'1 = error
+
+#endif
+
+#if SUPERO
+
+(+) = addInt'2
+(*) = mulInt'2
+(==) = eqInt'2
+(/=) = neqInt'2
+mod = modInt'2
+(-) = subInt'2
+error = error'1
+
 
 map f x = case x of
     y:ys -> f y : map f ys
     [] -> []
-
-filter f x = case x of
-    [] -> []
-    x:xs -> case f x of
-        True -> x : filter f xs
-        False -> filter f xs
 
 iterate f x = x : iterate f (f x)
 
 (!!) :: [a] -> Int -> a
 (!!) xs y = case xs of
     [] -> error "bad"
-    x:xs -> case eq y of
+    x:xs -> case y == 0 of
         True -> x
-        False -> (!!) xs (dec y)
+        False -> (!!) xs (y - 1)
 
-    
-    
+#endif

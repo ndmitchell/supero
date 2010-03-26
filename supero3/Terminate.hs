d5cee2e338408add385e0b4c0506b215037996d0
Neil Mitchell <ndmitchell@gmail.com>
2010-03-26 19:27:48 +0000

    Give lots of interesting information when terminating

--- supero3/Terminate.hs
+++ supero3/Terminate.hs
@@ -12,7 +12,13 @@ newHistory = History []
 
 
 terminate :: (Exp -> Exp -> Bool) -> History -> Exp -> Bool
-terminate (<) (History hist) x = if not $ all (x <) hist then True else False
+terminate (<) (History hist) x = if not $ all (x <) hist then info else False
+    where
+        bad = head $ filter (not . (x <)) hist
+        info = error $ prettyNames bad ++
+               "\n WHEN TRYING TO ADD\n" ++ prettyNames x ++
+               "\n BECAUSE OF\n" ++ show (getBag x \\ getBag bad) ++ "\n" ++
+               show ("<",x<bad,"==",x==bad,"bageq",getBag x == getBag bad,"<|",x<|bad,"<=|",x<=|bad)
     
 --    where
 --        info = error $ prettyNames (head hist) ++ "\n AGAINST \n" ++ prettyNames x ++ "\n" ++ show (getBag x ,getBag y)

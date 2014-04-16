e5c9aa57cd378e174701883578e157ad27536607
Neil Mitchell <ndmitchell@gmail.com>
2014-04-16 20:47:35 +0100

    Make MapMap capable of running and producing some output

--- supero4/Test/Simple/MapMap.hs
+++ supero4/Test/Simple/MapMap.hs
@@ -1,9 +1,14 @@
 
-module Main where
+module Test.Simple.MapMap(main) where
 
+#if MAIN
+main = print $ root (+1) (*2) [1,2,3]
+#endif
 
 root f g x = map f (map g x)
 
+#if SUPERO
 map f x = case x of
     [] -> []
     y:ys -> f y : map f ys
+#endif

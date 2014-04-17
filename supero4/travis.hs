3bc02a9ea3dd3658acdd4235a5f37ea5144a9bb7
Neil Mitchell <ndmitchell@gmail.com>
2014-04-17 13:03:11 +0100

    Fix up the travis.hs file

--- supero4/travis.hs
+++ supero4/travis.hs
@@ -3,5 +3,5 @@ import Neil
 
 main :: IO ()
 main = do
-    retry 3 $ "cabal install criterion"
+    retry 3 $ cmd "cabal install criterion"
     cmd "supero --compile --test --benchmark mapmap rev prims mapid iterate index digits_of_e2"

788822cf9820e8d7b50f414c54636f037e8f3451
Neil Mitchell <ndmitchell@gmail.com>
2014-04-19 09:59:46 +0100

    Use the nice idempotent method in simplify

--- supero4/Simplify.hs
+++ supero4/Simplify.hs
@@ -20,8 +20,7 @@ simplifys :: [(Var,Exp)] -> [(Var,Exp)]
 simplifys = map (second simplify)
 
 simplify :: Exp -> Exp
-simplify = \x -> let y = transform f x; yy = transform f y in if y == yy then x ~> y else
-                 error $ "Simplify not idempotent!\nOriginal\n" ++ pretty x ++ "\nFirst result\n" ++ pretty y ++ "\nSecond result\n" ++ pretty yy
+simplify = \x -> x ~> idempotent "simplify" fs x
     where
         fs = transform f
 

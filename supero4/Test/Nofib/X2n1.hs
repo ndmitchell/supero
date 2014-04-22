60a94c22b4c1b09cc3ecccb72bcf1e2cdd25dab0
Neil Mitchell <ndmitchell@gmail.com>
2014-04-22 07:49:47 +0100

    Make x2n1 exactly like it is in the nofib suite

--- supero4/Test/Nofib/X2n1.hs
+++ supero4/Test/Nofib/X2n1.hs
@@ -5,11 +5,11 @@ module Test.Nofib.X2n1(test) where
 import Data.Complex
 
 test = (\i -> root (i :: Int) :: Int, 80000 :: Int)
-
-operation :: Int -> Complex Double
-operation n = mkPolar 1 ((2*pi)/fromIntegral n) ^ n
 #endif
 
 #include "Include.hs"
 
-root n = round (realPart (sum (map operation (enumFromTo 1 n))))
+f :: Int -> Complex Double
+f n = mkPolar 1 ((2*pi)/fromIntegral n) ^ n
+
+root n = round (realPart (sum [f n | n <- [1 .. n]]))
